98b5798894e7e9e9981c8835777f1996a4019273
Claudia Alves <purchase@jbaptista.local>
2025-04-29 15:40:20 +0100

    Atualiza URL do backend no script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+const API_URL = 'https://agenda-backend-vr7o.onrender.com'; // domínio do Render
+
+fetch('${API_URL}/eventos')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+
 document.addEventListener('DOMContentLoaded', function () {
   const calendarEl = document.getElementById('calendar');
 
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
-    events: 'http://localhost:3000/eventos',
+    events: '${API_URL}/eventos', // <- aqui substituído
     selectable: true,
     select: function(info) {
       const title = prompt('Título do evento:');
       if (title) {
-        fetch('http://localhost:3000/eventos', {
+        fetch('${API_URL}/eventos', { // <- aqui também substituído
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
